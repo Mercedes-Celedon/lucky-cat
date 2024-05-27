80f71a6a97ac279b7c5ffd50f0bc8b5ab08cadf4
Flavia Ferrigno <flaviaferrigno@Flavias-MacBook-Pro.local>
2024-05-27 12:32:16 +0200

    arroyAdded

--- lucky_cat.js
+++ lucky_cat.js
@@ -1,11 +1,14 @@
-function addNewName(){
-
-  let name= document.getElementById("names");
-  let lista= document.getElementById("list_names")
-  const li = document.createElement("li");
+const addNewName=() => { 
+  let name= document.getElementById("names"); 
+  let lista= document.getElementById("list_names") 
+  const li = document.createElement("li"); 
+/*   if(name.value === ""){
+    alert("Type a name");
+  }else{ */
+  
   li.textContent= name.value;
-  name.value=""
- lista.appendChild(li)
-}
+  lista.appendChild(li) 
+  name.value="" 
+  }
 
 document.getElementById("button_names").addEventListener("click", addNewName)
