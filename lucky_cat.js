ecb860dfe71252efd982471a14fa17774f7e13c3
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-05-28 15:50:19 +0200

    Se agregan los valores del input a un array

--- lucky_cat.js
+++ lucky_cat.js
@@ -1,54 +1,51 @@
+let playersName = [];
 
-
-
-const addNewName=() => { 
-  let name= document.getElementById("names"); 
-  let lista= document.getElementById("list_names") 
-  const li = document.createElement("li"); 
-  if(name.value === ""){
+const addNewName = () => {
+  let name = document.getElementById("names");
+  let lista = document.getElementById("list_names");
+  const li = document.createElement("li");
+  if (name.value === "") {
     alert("Type a name");
-  }else{
-  
-  li.textContent= name.value;
+    return;
+  } else {
+    li.textContent = name.value;
   }
-  const deleteBtn = document.createElement('button');
-  deleteBtn.textContent = 'X';
-  deleteBtn.classList.add ('btn')
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.classList.add("btn");
+  deleteBtn.addEventListener("click", deleteName);
 
-  const editBtn = document.createElement('button');
-  editBtn.textContent = 'Y';
-  editBtn.classList.add("editB")
-  deleteBtn.addEventListener('click', deleteName );
+  //Consigo el value del input y le hago push al array para almacenar los nombres
+  let names = name.value;
+  playersName.push(names);
+  console.log(playersName);
 
- 
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "Y";
+  editBtn.classList.add("editB");
+
   //editBtn.addEventListener('click', editName )
-   
-  li.appendChild (deleteBtn)
-  li.appendChild (editBtn)
-  lista.appendChild(li) 
-  name.value="" 
-  
 
-  //guardado          = JSON.parse(localStorage.getItem('datos'));
-  
-  }
-  document.getElementById("button_names").addEventListener("click", addNewName)
+  li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
+  lista.appendChild(li);
+  name.value = "";
 
-   
-let namesPlay = []; 
-  namesPlay.push(name.value)
-  
-let deleteAll = document.querySelectorAll ('.btn')
+  //guardado= JSON.parse(localStorage.getItem('datos'));
+};
+document.getElementById("button_names").addEventListener("click", addNewName);
+
+
+
+
+let deleteAll = document.querySelectorAll(".btn");
 for (const btn of deleteAll) {
-  btn.addEventListener ('click', deleteName)
+  btn.addEventListener("click", deleteName);
 }
 
-function deleteName ( event ) {
-  let li = event.target.parentElement
-  li.remove () 
-
+function deleteName(event) {
+  let li = event.target.parentElement;
+  li.remove();
 }
 
-
-
-document.getElementById("button_names").addEventListener("click", addNewName)
+document.getElementById("button_names").addEventListener("click", addNewName);
