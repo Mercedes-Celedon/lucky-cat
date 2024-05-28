4e5b8fddd1c36b468f560d8f5d07d1e079ee1feb
Carlassanchez24 <carlassanchez24@gmail.com>
2024-05-28 13:01:27 +0200

    botondeletedone

--- lucky_cat.js
+++ lucky_cat.js
@@ -1,14 +1,50 @@
+
+
+
 const addNewName=() => { 
   let name= document.getElementById("names"); 
   let lista= document.getElementById("list_names") 
   const li = document.createElement("li"); 
-/*   if(name.value === ""){
+  if(name.value === ""){
     alert("Type a name");
-  }else{ */
+  }else{
   
   li.textContent= name.value;
+  }
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'X';
+  const editBtn = document.createElement('button');
+  deleteBtn.classList.add ('btn')
+  deleteBtn.addEventListener('click', deleteName );
+  editBtn.textContent = 'Y';
+ 
+  //editBtn.addEventListener('click', editName )
+   
+  li.appendChild (deleteBtn)
+  li.appendChild (editBtn)
   lista.appendChild(li) 
   name.value="" 
+  
+
+  //guardado          = JSON.parse(localStorage.getItem('datos'));
+  
   }
 
-document.getElementById("button_names").addEventListener("click", addNewName)
+  document.getElementById("button_names").addEventListener("click", addNewName)
+
+   
+let namesPlay = []; 
+  namesPlay.push(name.value)
+  
+let deleteAll = document.querySelectorAll ('.btn')
+for (const btn of deleteAll) {
+  btn.addEventListener ('click', deleteName)
+}
+
+function deleteName ( event ) {
+  let li = event.target.parentElement
+  li.remove () 
+
+}
+
+
