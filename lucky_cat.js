d29192be110423fcfc707a95e90e2e77b2a12582
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-05-29 12:56:33 +0200

    Configuracion botón borrar y editar un nombre de la lista

--- lucky_cat.js
+++ lucky_cat.js
@@ -15,51 +15,50 @@ const addNewName = () => {
   deleteBtn.classList.add("btn");
   deleteBtn.addEventListener("click", deleteName);
 
-  const editBtn = document.createElement('button');
-  editBtn.textContent = 'Y';
-  editBtn.classList.add('btn-edit')
-  editBtn.addEventListener('click', editName )
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "Y";
+  editBtn.classList.add("btn-edit");
+  editBtn.addEventListener("click", editName);
 
   //Consigo el value del input y le hago push al array para almacenar los nombres
   let names = name.value;
   playersName.push(names);
   console.log(playersName);
 
+  //Agrega un atributo al elemento li, después que lo agregamos al array
+  li.setAttribute("data-position", playersName.length - 1);
+
   li.appendChild(deleteBtn);
   li.appendChild(editBtn);
   lista.appendChild(li);
   name.value = "";
-
 };
 document.getElementById("button_names").addEventListener("click", addNewName);
 
 
-
-
-let deleteAll = document.querySelectorAll(".btn");
-for (const btn of deleteAll) {
-  btn.addEventListener("click", deleteName);
-}
-  let editAll=document.querySelectorAll('.btn-edit')
-for (const btn of editAll){
-  btn.addEventListener('click', editName)
-
+let editAll = document.querySelectorAll(".btn-edit");
+for (const btn of editAll) {
+  btn.addEventListener("click", editName);
 }
 
 function deleteName(event) {
   let li = event.target.parentElement;
+  let position = li.getAttribute("data-position");
+  playersName.splice(position, 1);
+  li.remove();
+  console.log(playersName);
+}
+
+function editName(event) {
+  let name = document.getElementById("names");
+  let li = event.target.parentElement;
+  let position = li.getAttribute("data-position");
+  playersName.splice(position, 1);
+  let nameInner = li.innerText;
+  console.log(nameInner);
+  name.value = nameInner;
+  console.log(name.textContent);
   li.remove();
 }
-
-function editName(event){
-  let name= document.getElementById("names");
-  let li= event.target.parentElement;
-  let nameInner=li.innerText;
-  console.log(nameInner)
-  name.value= nameInner;
-  console.log(name.textContent)
-  li.remove()
-}
-
 
 document.getElementById("button_names").addEventListener("click", addNewName);
