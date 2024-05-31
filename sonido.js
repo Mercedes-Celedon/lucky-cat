f13d996bba053535e8f4034b514941d83c7ea51d
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-05-31 15:46:17 +0200

    Cambios en sonido.js

--- sonido.js
+++ sonido.js
@@ -1,19 +1,29 @@
+const slideContainer = document.getElementById("slide-container");
+slideContainer.classList.add("color");
+document.addEventListener("DOMContentLoaded", function () {
+  const checkbox = document.getElementById("slide");
+  const audio = document.getElementById("audio");
+  //audio.play();
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      audio.play();
+      slideContainer.classList.add("color");
+    } else {
+      audio.pause();
+      slideContainer.classList.add("color-off");
+    }
+  });
+});
 
-const slideContainer = document.getElementById('slide-container')
-slideContainer.classList.add('color-off')
-document.addEventListener('DOMContentLoaded', function() {
-    const checkbox = document.getElementById('slide');
-    const audio = document.getElementById('audio');
-    //audio.play();
-    checkbox.addEventListener('change', function() {
-        if (checkbox.checked) {
-            audio.play();
-            slideContainer.classList.add('color')
-        } else {
-            audio.pause();
-            slideContainer.classList.add('color-off')
-        }
-    });
+var catAudio = new Audio("./sounds/sound-cat-brr.mp3");
+const catFace = document.querySelector(".cat-head");
+
+catFace.addEventListener("mouseover", function () {
+  catAudio.play();
+});
+catFace.addEventListener("mouseout", function () {
+  catAudio.pause();
+  catAudio.currentTime = 0;
 });
 
 //usamos el DOMContentLoaded para asegurarse de que el DOM esté completamente cargado antes de ejecutar el código.
